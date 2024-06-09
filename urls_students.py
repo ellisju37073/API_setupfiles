98b33c6416ea2876a626e79558badd2006cb8d9a
ellisju37073 <67281488+ellisju37073@users.noreply.github.com>
2024-06-09 12:07:36 -0700

    Update urls_students.py

--- urls_students.py
+++ urls_students.py
@@ -1,8 +1,12 @@
-from .views import StudentView
-from .views import StudentView1
-from django.urls import path
+from .views import StudentView  # Importing the StudentView class from views
+from .views import StudentView1  # Importing the StudentView1 class from views
+from django.urls import path  # Importing the path function from django.urls
 
+# Define URL patterns for the API endpoints
 urlpatterns = [
+    # URL pattern for accessing the basic student view, which handles all students
     path('basic/', StudentView.as_view()),
+    
+    # URL pattern for accessing a specific student view based on the student's ID
     path('basic/<int:id>/', StudentView1.as_view())
-]  
+]
